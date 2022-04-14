446eadb50d4173ac1e91e0189bf7c57ae5675b20
Georgian123123 <georgiancomana1119@gmail.com>
2022-04-14 20:30:18 +0300

    Laborator6: Solved TODO2 for both files
    Signed-off-by: Georgian123123 <georgiancomana1119@gmail.com>

--- tools/labs/skels/memory_mapping/kmmap/kmmap.c
+++ tools/labs/skels/memory_mapping/kmmap/kmmap.c
@@ -53,9 +53,15 @@ static int my_release(struct inode *inode, struct file *filp)
 static int my_read(struct file *file, char __user *user_buffer,
 		size_t size, loff_t *offset)
 {
+	int err;
 	/* TODO 2: check size doesn't exceed our mapped area size */
+	if (size > NPAGES * PAGE_SIZE)
+		return -ENOMEM;
 
 	/* TODO 2: copy from mapped area to user buffer */
+	err = copy_to_user((void *)user_buffer, kmalloc_area, size);
+	if (err)
+		return -1;
 
 	return size;
 }
@@ -63,9 +69,14 @@ static int my_read(struct file *file, char __user *user_buffer,
 static int my_write(struct file *file, const char __user *user_buffer,
 		size_t size, loff_t *offset)
 {
-	/* TODO 2: check size doesn't exceed our mapped area size */
+	int err;
+		/* TODO 2: check size doesn't exceed our mapped area size */
+	if (size > NPAGES * PAGE_SIZE)
+		return -ENOMEM;
 
-	/* TODO 2: copy from user buffer to mapped area */
+	/* TODO 2 : copy from user buffer to mapped area */
+	err = copy_from_user((void *)user_buffer, kmalloc_area, size);
+
 
 	return size;
 }
